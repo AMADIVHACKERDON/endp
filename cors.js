3a801a0c2c57cfb2f7b5cfcba7a4c86880c4fba7
AMADIVHACKERDON <101997952+AMADIVHACKERDON@users.noreply.github.com>
2025-01-31 20:13:03 +0100

    Add files via upload

---
+++ cors.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const core = require('cors');
+const { logEvents } = require('./middleware/logEvent');
+const { timeStamp } = require('console');
+
+
+
+// const whitelist = ['https://www.yoursite.com', 'https://127.0.0.1:5500','https://localhost:3500']
+//    const coreoption = {
+  //      origin: (origin, callback) =>{
+    //        if (whitelist.indexOf(origin) != -1 || !origin) {
+      //          callback(null, true)
+        //    } else{
+          //      callback(new Error('Not Allowedby CORS'))
+            //}
+        //},
+        //optionsSuccessStatus: 200
+    //}
+app.use(core())
+
+
+
+app.get('^/', (req,res) => {
+    res.json({
+        slack_email: "victory.amadi@stu.cu.edu.ng",
+        timestamp: new Date().toISOString(),
+        github_repo: "https://github.com/AMADIVHACKERDON?tab=repositories"
+    })
+    res.status(200);
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
